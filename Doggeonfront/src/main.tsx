36f5464bed42c6331b26b788f9bb60c1dfca8e42
Ivan <vanas0576@gmail.com>
2025-09-29 15:50:52 +0500

    Фронтенд обнулен

--- Doggeonfront/src/main.tsx
+++ Doggeonfront/src/main.tsx
@@ -3,17 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors ({
-    origin: "http://localhost:5173",
-    methods: 'GET,POST,PUT,PATCH,HEAD,DELETE'
-    credentials: true,
-  });
-  await app.listen(3000);
-}
-bootstrap();
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
